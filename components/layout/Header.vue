ff6676fc8a3ad99e7d0c0bb0150ab0b22cd4742a
shirinzadeh <netzadeh@gmail.com>
2024-09-19 19:12:53 +0400

    fixed navigation menu

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -2,8 +2,8 @@
   <header class="bg-gray-800 text-white py-4">
     <div class="container mx-auto px-4 flex justify-between items-center">
       <NuxtLink to="/" class="text-2xl font-bold">Casino Hotels</NuxtLink>
-      <NavigationMenu class="hidden md:block" />
-      <HamburgerIcon @click="toggleSidebar" class="md:hidden" />
+      <NavigationMenu class="hidden sm:block" />
+      <HamburgerIcon @click="toggleSidebar" class="sm:hidden" />
     </div>
   </header>
   <Sidebar :isOpen="isSidebarOpen" @close="toggleSidebar" />
--- components/navigation/NavigationMenu.vue
+++ components/navigation/NavigationMenu.vue
@@ -1,6 +1,6 @@
 <template>
     <nav>
-      <ul class="flex mt-8 flex-col space-y-4">
+      <ul class="flex mt-8 flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 sm:mt-0">
         <li v-for="item in menuItems" :key="item.path">
           <NuxtLink :to="item.path" class="hover:text-gray-300">{{ item.name }}</NuxtLink>
         </li>
