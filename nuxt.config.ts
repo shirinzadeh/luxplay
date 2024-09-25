b30805875698c8b0356c42dd14329f2ff6d075be
shirinzadeh <netzadeh@gmail.com>
2024-09-25 16:24:40 +0400

    added vercel.json to fix security problems

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,8 +22,10 @@ export default defineNuxtConfig({
 ],
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ]
     }
   },
   css: [
